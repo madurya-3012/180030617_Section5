774c3253f8edf03d0ad588fcf2bdc58495431a4d
madurya-3012 <madurya3012@gmail.com>
2021-05-12 11:36:17 +0530

    Commit

---
+++ ClassAndObject/src/First.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class First {
+	double h,w,d;
+
+    First(double width,double height,double depth)
+   {
+       h=height;
+       w=width;
+       d=depth;
+   }
+   double volume()
+   { double v;
+   v=h*w*d;
+   return v;
+   }
+  
+   public static void main(String[] args) {
+      
+   First bc = new First(8.5,80.3,9.6);
+   System.out.println(bc.volume());
+
+   }
+}

7f296ed763784ffa4f1295ad26c1521ca790433a
madurya-3012 <madurya3012@gmail.com>
2021-05-12 09:34:37 +0530

    Commit

---
+++ LoopsQ/src/Nineteen.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Nineteen {
+	public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);        
+        int N=sc.nextInt();
+        for (int num = 0; num < N; num++)
+        {    
+           
+            if (num % 3 == 0 && num % 5 == 0)
+                System.out.print(num + " ");
+        }
+        sc.close();
+    }
+}
